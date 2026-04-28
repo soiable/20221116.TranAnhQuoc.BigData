fd81200be310763e9fb47603e8345a305a347e9c
soiable <20221116@eaut.edu.vn>
2026-04-28 15:22:45 +0700

    producer.py

--- producer.py
+++ producer.py
@@ -2,27 +2,26 @@ from kafka import KafkaProducer
 import gzip
 import time
 
-# ==============================
+
 # CONFIG
-# ==============================
+
 KAFKA_TOPIC = "pageviews"
 KAFKA_SERVER = "localhost:9092"
-file_path = "../data/pageviews-20260101-000000.gz"   # <-- sửa nếu file nằm chỗ khác
-DELAY = 0.01                # thời gian delay giữa mỗi record
+file_path = "../data/pageviews-20260101-000000.gz"
+DELAY = 0.01
 
-# ==============================
+
 # CREATE PRODUCER
-# ==============================
+
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_SERVER,
     value_serializer=lambda v: v.encode("utf-8")
 )
 
-print("🚀 Start streaming data to Kafka...")
+print("Start streaming data to Kafka...")
 
-# ==============================
 # READ .GZ FILE & SEND TO KAFKA
-# ==============================
+
 try:
     with gzip.open(file_path, "rt", encoding="utf-8") as f:
         for i, line in enumerate(f):
@@ -32,14 +31,12 @@ try:
             if not line:
                 continue
 
-            # gửi vào Kafka
+
             producer.send(KAFKA_TOPIC, value=line)
 
-            # log ra để debug
             if i % 100 == 0:
                 print(f"Sent {i} records...")
 
-            # giả lập streaming
             time.sleep(DELAY)
 
 except FileNotFoundError:
